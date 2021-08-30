4efa7592468a68d84a31cf3a12c744d270b79ab7
Michele Cancilla <cancilla.michele@gmail.com>
2021-08-30 17:13:36 +0200

    Update pyecvl_install.sh

--- scripts/pyecvl_install.sh
+++ scripts/pyecvl_install.sh
@@ -13,7 +13,6 @@ git clone --recurse-submodules --jobs 2 \
   https://github.com/deephealthproject/pyecvl.git pyecvl_${PYECVL_TAG} --branch ${PYECVL_TAG}
 cd pyecvl_${PYECVL_TAG}
 
-git checkout tags/"${PYECVL_TAG}"
 PYECVL_ROOT=$(pwd)
 
 # eddl and PyEDDL
